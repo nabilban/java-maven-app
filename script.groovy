0ee01b1488e07876c38113ef09443b64c300f030
M Farhan Nabil <120365344+nabilban@users.noreply.github.com>
2025-10-08 13:40:41 +0700

    Update Docker image and repository names

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "Building the Docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t azeshion21/demo-app:jma-2.0 .'
+        sh 'docker build -t nabilban/demo-app:jma-2.0 .'
         sh "echo \$PASS | docker login -u \$USER --password-stdin"
-        sh 'docker push azeshion21/demo-app:jma-2.0'
+        sh 'docker push nabilban/demo-app:jma-2.0'
     }
 }
 
